aae68a8532a56b862c35fe9e1137ed8fb56f7a00
liton78 <91829066+liton78@users.noreply.github.com>
2021-11-26 15:17:14 +0300

    add

---
+++ Liton-HW-C-33/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    'use strict'
+    // for banner
+    $('.banner-slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+    });
+
+    // venubox about
+    $('.venobox').venobox();
+});
